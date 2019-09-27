ff9f952647918ba77d9b88c0181de59bc9f01166
nettorammos@hotmail.com <Digital@DH-A10-35.dhbrasil.digitalhouse.com>
2019-09-27 11:56:58 -0300

    Estrutura de Pastas da loja

--- index.php
+++ index.php
@@ -1,34 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<?php
+        $title = "Fruit Phones";
+    ?>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>
+    <?php echo $title ?>
+    </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    <?php
-        $nome = "José";
-        $idade = 18;
-        $sobrenome = "Ramos";
-        $mochila = [
-            "Óculos",
-            "Estojo",
-            "Caderno"
-            
-        ];
-        $cliente = [
-            "nome" => "Julia",
-            "sobrenome" => "Soares",
-            "idade" => "123",
-            "saldo" => "-870"
-
-        ];
-
-
-         var_dump( $mochila) ;
-         echo $cliente["saldo"];
-    ?>
+   
+    <header class="bg-light">
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-md-6 col-lg-6 p-3  ">
+            <h1 class="h5"><?php echo $title ?></h1>
+        </div>
+        <div class="col-md-6 col-lg-6 d-flex justify-content-end">
+            <nav>
+                <ul class="nav">
+                <li class="nav-item p-3"><a href="" class="link-item text-dark">Cursos</a></li>
+                <li class="nav-item p-3"><a href="" class="link-item text-dark">Login</a></li>
+                <li class="nav-item p-3"><a href="" class="link-item text-dark">Cadastro</a></li>
+                </ul>
+            </nav>
+        </div>
+        </div>
+    </div>
+</header>
 
 </body>
 </html>
